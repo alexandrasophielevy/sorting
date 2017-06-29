f9ca811cdf77c915e614c523d33e6fda4378747d
Alexandra Levy <alexandrasophielevy@gmail.com>
2017-06-29 15:17:05 +0000

    completed sorting exercises

---
+++ advancedsorting.js
@@ -0,0 +1,21 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+students.sort(function(a, b) {
+  var nameA = a.name.toLowerCase();
+  var nameB = b.name.toLowerCase();
+  if(nameA < nameB) {
+    return -1;
+  }
+  if(nameA > nameB) {
+    return 1;
+  }
+
+  return a.age - b.age
+});
+
+console.log(students);
+
